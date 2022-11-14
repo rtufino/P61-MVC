ce4669b373fd42e119df68211a5da1166cfa5b39
Rodrigo Tufiño <rtufino@ups.edu.ec>
2022-11-14 10:09:11 -0500

    Cambiar notas del estudiante

--- vista.cpp
+++ vista.cpp
@@ -8,12 +8,17 @@ Vista::Vista()
 void Vista::actualizarNota()
 {
     int nota = 0;
+    int valor;
     do {
-        cout << "Ingrese la nota 1: ";
+        cout << "Ingrese la nota a modificar: ";
         cin >> nota;
-    }while (nota < 0 || nota > 10);
-    controlador->actualizaNota(1, nota);
-    cout << "Nota 1 actualizada!" << endl;
+    }while (nota < 0 || nota > 3);
+    do {
+        cout << "Ingrese el nuevo valor de la nota: ";
+        cin >> valor;
+    }while (valor < 0 || valor > 10);
+    controlador->actualizaNota(nota, valor);
+    cout << "Nota "<< nota <<" actualizada!" << endl;
 }
 
 void Vista::imprimir()
